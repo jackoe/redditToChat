1794bba6bb61f7b30dc5a477833d75846a38c463
jackoe <jackwines@mac.com>
2017-03-07 01:52:27 -0600

    modified lib.hs, added but didn't integrate groupme

--- src/lib.hs
+++ src/lib.hs
@@ -9,6 +9,7 @@ import Control.Monad.IO.Class
 import Data.Monoid
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
+import qualified Data.Text.Internal as IntText
 
 getLink :: PostContent -> Text.Text
 getLink (SelfPost _ x) = x
@@ -19,8 +20,16 @@ getLink TitleOnly = Text.pack "Just the title"
 subRedditPosts :: RedditT IO PostListing
 subRedditPosts = getPosts' (Options {pagination = Nothing, limit = Just 1}) Hot (Just . R . Text.pack $  "wallpapers")
 
+extractPostFromListing :: Listing t Post -> IntText.Text
+extractPostFromListing (Listing _ _ posts) = extractPost . head $ posts
 
+extractPost :: Post -> IntText.Text
 extractPost = getLink . content
+
+theLink :: RedditT IO Text.Text
+theLink = fmap extractPostFromListing subRedditPosts
+
+-- printTheLink = liftIO . Text.putStrLn
 
 messageTest = runRedditAnon $ do
     Listing _ _ posts <- subRedditPosts
